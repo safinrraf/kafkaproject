1470f853598d961a90da3724c94d506e2352c57a
Radik <safinrraf@ymail.com>
2023-01-20 08:49:28 +0100

    init commit

---
+++ src/main/java/com/kafka/learning/kafkaproject/KafkaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.learning.kafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/learning/kafkaproject/KafkaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.learning.kafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
