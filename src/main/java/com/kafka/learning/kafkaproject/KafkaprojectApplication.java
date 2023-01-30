a98b68a0078678169335f4b5bd73392ddc4ead42
Radik <safinrraf@ymail.com>
2023-01-30 11:34:18 +0100

    first working version

--- src/main/java/com/kafka/learning/kafkaproject/KafkaprojectApplication.java
+++ src/main/java/com/kafka/learning/kafkaproject/KafkaprojectApplication.java
@@ -2,12 +2,18 @@ package com.kafka.learning.kafkaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class KafkaprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaprojectApplication.class, args);
+	}
+
+	@KafkaListener(id = "myId", topics = "local_topic")
+	public void listen(String in) throws Exception{
+		System.out.println(in);
 	}
 
 }
